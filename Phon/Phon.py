53e422c8775972c86e8efcb8a97acb7eea01982a
steeve <barthelemysteevebernard@gmail.com>
2019-10-08 22:06:19 -0300

    Ajustado para criar a tabela de conjunto de instrucoes  Mudanças a serem submetidas: 	modified:   Phon/Phon.py 	new file:   Phon/README.MD 	modified:   Phon/dataSample/data.txt

--- Phon/Phon.py
+++ Phon/Phon.py
@@ -29,25 +29,135 @@ class GenericAplication():
         return text
 
 
-    def programaBinaria(self):
-        pgbinary = []
+    def __getRotulo(self, name, objRot):
+        for rot in objRot:
+            if name == rot['rotulo']:
+                return rot['rotulo']                           
+        return 'No Existe este rotulo'
+
+    def __conjInstrucoes(self, name):        
+        conjinstru = [{
+            'opcode' : '00',
+            'name': 'NOP',
+            'tamanho': 1
+        },{
+            'opcode' : '10',
+            'name': 'LDR',
+            'tamanho': 2
+        },{
+            'opcode' : '20',
+            'name': 'STR',
+            'tamanho': 2
+        },{
+            'opcode' : '30',
+            'name': 'ADD',
+            'tamanho': 2
+        },{
+            'opcode' : '40',
+            'name': 'SUB',
+            'tamanho': 2
+        },{
+            'opcode' : '50',
+            'name': 'MUL',
+            'tamanho': 2
+        },{
+            'opcode' : '60',
+            'name': 'DIV',
+            'tamanho': 2
+        },{
+            'opcode' : '70',
+            'name': 'NOT',
+            'tamanho': 1
+        },{
+            'opcode' : '80',
+            'name': 'AND',
+            'tamanho': 2
+        },{
+            'opcode' : '90',
+            'name': 'OR',
+            'tamanho': 2
+        },{
+            'opcode' : '100',# de A0  para cima
+            'name': 'XOR',
+            'tamanho': 2
+        },{
+            'opcode' : '110',
+            'name': 'JMP',
+            'tamanho': 2
+        },{
+            'opcode' : '120',
+            'name': 'JEQ',
+            'tamanho': 2
+        },{
+            'opcode' : '130',
+            'name': 'JG',
+            'tamanho': 2
+        },{
+            'opcode' : '140',
+            'name': 'JL',
+            'tamanho': 2
+        },{
+            'opcode' : '150',
+            'name': 'HLT',
+            'tamanho': 1
+        }]
+
+        for inst in conjinstru:            
+            if name == inst['name']:                                
+                return inst
+        return None
+
+    def programaBinaria(self):        
+        simbolData = []
+        instrucoes = []
+        passo = 0
 
         fileData = open(self.arquivo, 'r')
         lines  = fileData.readlines()
         
         for x in range(2):
-
             if x==0:
-                for l in lines:
-                    # print(l.split())
-                    result = re.search(':', l)
-                    if result != None:
-                        print(self.__splitGetData(l,':',0))        
+                # Primeira pasagem pegando os rotulos e o endereço da memoria
 
-                fileData.close()
-                print('Primeiro paso ',x);
-            else:                
-                print('Segundo paso ',x);
+                for l in lines: 
+                    if (len(l.split()) == 1) and (l.split()[0] == 'text') and (endAtual == 0):
+                        endAtual = 0                         
+                    if (len(l.split()) == 1) and (l.split()[0] == 'data'):
+                        endAtual = 128 
+
+                    if (len(l.split()) == 1) and (l.split()[0] != 'text') and (l.split()[0] != 'data'):
+                        print(endAtual,' - ',l.split()[0])
+                        if endAtual >= 128:
+                            endAtual += 1;
+                        else:
+                            endAtual += 2;
+
+
+                    elif (len(l.split()) == 1) and (l.split()[0] == 'data'):
+                        endAtual = 128
+                    
+                    elif (len(l.split()) > 1):                                                                    
+                        result = re.search(':', l)
+                        if result != None:                            
+                            print(endAtual,' - ',self.__splitGetData(l,':',0))                      
+                            simbolData.append({
+                                "rotulo" : self.__splitGetData(l,':',0),
+                                "end" : 5
+                            })
+                        else:
+                            for g in l.split():
+                                print(endAtual,' - ',g)
+                                if endAtual >= 128:
+                                    endAtual += 1;
+                                else:
+                                    endAtual += 2;
+                            # print(l.split())
+                fileData.close()                
+                # print(self.__getRotulo('loop', simbolData))            
+                print(self.__conjInstrucoes('STR'))                
+            else: 
+                # Conteudo da segunda pasagem no codigo                
+                pass
 
 
 if __name__=='__main__':
@@ -59,3 +169,4 @@ if __name__=='__main__':
 
     # Example
     # python Phon.py --arq data.txt
+# print(len(l.split()),' - ',l.split())                                        
